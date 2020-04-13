38040ae646bb603195ed0877fc20dfc8e70b7a2a
Saba <sabaazam306@gmail.com>
2020-04-13 07:59:56 +0500

    Updates

---
+++ precache-manifest.5617a339780dacd9ef3fc80741710c89.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "15a99ed1ec5e477adb49",
+    "url": "/mobile-flashcards/static/js/app.70eeb687.chunk.js"
+  },
+  {
+    "revision": "a64a255126ff467184d5",
+    "url": "/mobile-flashcards/static/js/runtime~app.9d84807a.js"
+  },
+  {
+    "revision": "97dea4bc567f2e27304f",
+    "url": "/mobile-flashcards/static/js/2.d4702cab.chunk.js"
+  },
+  {
+    "revision": "6165c9d7a2e729ba57b23dd93add5366",
+    "url": "/mobile-flashcards/static/media/back-icon-mask.6165c9d7.png"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/mobile-flashcards/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/mobile-flashcards/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/mobile-flashcards/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/mobile-flashcards/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "6beba7e6834963f7f171d3bdd075c915",
+    "url": "/mobile-flashcards/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "c6aef942e3668158ec29d4adcb2e768f",
+    "url": "/mobile-flashcards/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "872545dde71de3842234bf6afe80c4cb",
+    "url": "/mobile-flashcards/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/mobile-flashcards/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "5a293a273bee8d740a045d9922b9a9ae",
+    "url": "/mobile-flashcards/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/mobile-flashcards/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/mobile-flashcards/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/mobile-flashcards/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/mobile-flashcards/expo-service-worker.js"
+  },
+  {
+    "revision": "7018334e604031c8d4626daa248eb6f4",
+    "url": "/mobile-flashcards/favicon-16.png"
+  },
+  {
+    "revision": "d199d737deaef0c2af33b42cb9330fba",
+    "url": "/mobile-flashcards/favicon-32.png"
+  },
+  {
+    "revision": "c2d86908f297173a86bd73810a826f0d",
+    "url": "/mobile-flashcards/favicon.ico"
+  },
+  {
+    "revision": "a275a1678803a40d9e51eb531b2effb9",
+    "url": "/mobile-flashcards/index.html"
+  },
+  {
+    "revision": "a047a30c897b294db7153dee9e36542a",
+    "url": "/mobile-flashcards/manifest.json"
+  },
+  {
+    "revision": "12a621cdd2d598767432d18022690fb4",
+    "url": "/mobile-flashcards/register-service-worker.js"
+  }
+];
